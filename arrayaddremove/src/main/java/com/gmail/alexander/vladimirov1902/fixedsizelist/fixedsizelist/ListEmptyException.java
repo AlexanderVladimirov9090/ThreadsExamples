37b1a2e03e6e894d0150a84e828cb8cb923f47fe
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2017-09-26 13:06:53 +0300

    Made some changes and format code.

---
+++ arrayaddremove/src/main/java/com/gmail/alexander/vladimirov1902/fixedsizelist/fixedsizelist/ListEmptyException.java
@@ -0,0 +1,14 @@
+package com.gmail.alexander.vladimirov1902.fixedsizelist.fixedsizelist;
+
+/**
+ * @author Alexander Vladimirov
+ *         <alexandervladimirov1902@gmail.com>
+ *         Custom exception when list is empty.
+ */
+
+public class ListEmptyException extends RuntimeException {
+
+    public ListEmptyException(String message) {
+        super(message);
+    }
+}
---
+++ timeouttable/src/main/java/com/gmail/alexander/vladimirov1902/fixedsizelist/cachetable/LifeSpan.java
@@ -0,0 +1,18 @@
+package com.gmail.alexander.vladimirov1902.fixedsizelist.cachetable;
+
+/**
+ * Created by zumba on 11.08.16.
+ *
+ * @author Alexander Vladimirov
+ *         <alexandervladimirov1902@gmail.com>
+ */
+
+class LifeSpan {
+    public final long startTime;
+    public final long lifeSpan;
+
+    LifeSpan(long startTime, long lifeSpan) {
+        this.startTime = startTime;
+        this.lifeSpan = lifeSpan;
+    }
+}
